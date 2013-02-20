bfdec88cce0275756dcffc01189e05a5658946f0
giesk018 <giesk018@rynite.morris.umn.edu>
2013-02-20 12:58:47 -0600

    Made a working PrimeExperiment with runner. The tweak method needs to be smarter though, and the quality method as well.

--- EssentialsOfMetaheuristics/src/problems/PrimeEvolution.groovy
+++ EssentialsOfMetaheuristics/src/problems/PrimeEvolution.groovy
@@ -1,5 +1,34 @@
 package problems
 
-def parent1 = [1, 1, 1, 1 ,1 ,1 ,1 ,1]
-def parent2 = [0, 0, 0 ,0 ,0 ,0 ,0 ,0]
+//import groovy.transform.ToString
 
+class PrimeEvolution extends BitStringProblem {
+	def maximalQuality = {
+		Math.pow(2, numBits)
+	}
+	
+	def quality = { a ->
+		++evalCount
+		def total = 0
+		def pos = 1
+		for (i in a) {
+			if (i == 1) {
+				total += Math.pow(2, numBits - pos)
+			}
+			pos++
+		}
+		
+		if (total < 2) {
+			return total
+		} else if (total % 2 == 0) {
+			return 2
+		} else {
+			for (int i = 3; i <= Math.pow(2, numBits / 2); i += 2) {
+				if (total % i == 0) {
+					return i
+				}
+			}
+			return total
+		}
+	}
+}
